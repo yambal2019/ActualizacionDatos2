35f3ee2392598bbad75be25b4deb4f4590bce682
Luis Cano <canomartinezluis@gmail.com>
2019-08-20 10:15:41 -0500

    Carga Inicial
    es la carga inicial del proyecto

---
+++ Controllers/DatosController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActualizacionDatosCampaña.Controllers
+{
+    public class DatosController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(FormCollection formCollection)
+        {
+
+
+            return View();
+        }
+
+
+    }
+}
